d3e3663bc1e51c8206439308089f84ab17071d37
_0x0_P <zero1105x@outlook.com>
2022-03-24 04:18:50 +0900

    tiramisu
    Signed-off-by: _0x0_P <zero1105x@outlook.com>

--- web/pages/[docs]/index.tsx
+++ web/pages/[docs]/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next'
 import { useEffect } from 'react'
-import styles from '../styles/index.module.css'
+import styles from '../../styles/index.module.css'
 import ReactHtmlParser from 'react-html-parser';
-import { parser } from '../assets/parser';
+import { parser } from '../../assets/parser';
 import Link from 'next/link';
 import { useRouter } from 'next/router'
 
@@ -11,14 +11,15 @@ const data = `{"Import":"abc"} asdfasfdsfsdfsa {"Import":"abc"} {"Import":"abc"}
 
 const Home: NextPage = () => {
   const router = useRouter()
-  const { doc } = router.query
+  const { docs } = router.query
   useEffect(() => {
     })
   return (
     <div className={styles.container}>
-              <div className={styles.toolsContainer}></div>
+              <div className={styles.toolsContainer2}>
+              </div>
       <div className={styles.main}>
-      <Link href={`/${doc}`}><a className={styles.title} ><h1  style={{fontSize : 35}}>{doc}</h1></a></Link>
+      <Link href={`/${docs}`}><a className={styles.title} ><h1  style={{fontSize : 35}}>{docs}</h1></a></Link>
       {ReactHtmlParser(parser(data))}
 <h2>
 
